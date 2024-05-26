e30bc30dfd4eaadb87f44dbcd3a1d0059d686652
Danylo Koval <37926017+PolinianCode@users.noreply.github.com>
2024-05-26 17:28:08 +0200

    Creating Broker Class with handles messages and process them
    handle functions are empty. Just a structure of message processing and handling

--- MQTT-Broker/Broker.cpp
+++ MQTT-Broker/Broker.cpp
@@ -1,1 +1,25 @@
 #include "Broker.h"
+#include "MQTTMessageTypes.cpp"
+#include <iostream>
+#include <vector>
+
+void Broker::dispatchMessage(const std::vector<unsigned char>& message) {
+	unsigned char messageType = message[0];
+    switch (messageType) {
+        case MQTT_MSG_CONNECT:
+            handleConnect(message);
+            break;
+        case MQTT_MSG_PUBLISH:
+            handlePublish(message);
+            break;
+        case MQTT_MSG_SUBSCRIBE:
+            handleSubscribe(message);
+            break;
+        case MQTT_MSG_PINGREQ:
+            handlePing(message);
+            break;
+        default:
+            std::cerr << "Unknown message type" << std::endl;
+            break;
+    }
+}
--- MQTT-Broker/Broker.h
+++ MQTT-Broker/Broker.h
@@ -1,5 +1,16 @@
 #pragma once
+
+#include <vector>
+#include <string>
+
 class Broker
 {
+public:
+	void dispatchMessage(const std::vector<unsigned char>& message);
+private:
+    void handleConnect(const std::vector<unsigned char>& message);
+    void handlePublish(const std::vector<unsigned char>& message);
+    void handleSubscribe(const std::vector<unsigned char>& message);
+    void handlePing(const std::vector<unsigned char>& message);
 };
 
