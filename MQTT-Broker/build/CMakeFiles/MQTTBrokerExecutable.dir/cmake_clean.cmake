1d3eeca776b72d981d04d61d52db6fcd71db46d4
Danylo Koval <37926017+PolinianCode@users.noreply.github.com>
2024-06-03 19:05:03 +0200

    Cmake inited

---
+++ MQTT-Broker/build/CMakeFiles/MQTTBrokerExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MQTTBrokerExecutable.dir/Broker.cpp.o"
+  "CMakeFiles/MQTTBrokerExecutable.dir/Broker.cpp.o.d"
+  "CMakeFiles/MQTTBrokerExecutable.dir/MQTT-Broker.cpp.o"
+  "CMakeFiles/MQTTBrokerExecutable.dir/MQTT-Broker.cpp.o.d"
+  "CMakeFiles/MQTTBrokerExecutable.dir/MQTTMessageTypes.cpp.o"
+  "CMakeFiles/MQTTBrokerExecutable.dir/MQTTMessageTypes.cpp.o.d"
+  "CMakeFiles/MQTTBrokerExecutable.dir/TCPServer.cpp.o"
+  "CMakeFiles/MQTTBrokerExecutable.dir/TCPServer.cpp.o.d"
+  "CMakeFiles/MQTTBrokerExecutable.dir/main.cpp.o"
+  "CMakeFiles/MQTTBrokerExecutable.dir/main.cpp.o.d"
+  "MQTTBrokerExecutable"
+  "MQTTBrokerExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MQTTBrokerExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MQTT-Broker/build/CMakeFiles/MQTTBrokerExecutable.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MQTTBrokerExecutable.
+# This may be replaced when dependencies are built.
